5068374e5bae6d9bdcb4ef4b31cf69d4b7b8219a
Caddy Dz <tbitw31@gmail.com>
2017-09-03 10:11:23 +0100

    Remove duplicate password entry in postgresql database config
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- config/database.php
+++ config/database.php
@@ -73,7 +73,6 @@ return [
             'database' => $database,
             'username' => $username,
             'password' => $password,
-            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
